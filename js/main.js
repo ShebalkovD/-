997130e26bd74f982b2bd76003690790ca411d00
ShebalkovD <shebalkovdenis.wk@gmail.com>
2021-11-24 22:22:01 +0300

    js-logic

--- js/main.js
+++ js/main.js
@@ -1,10 +1,34 @@
-$(function(){
+const navItem  = document.getElementById('ndpm')
+const dpMenuList = document.getElementById('dp')
+const navItemDelivery = document.getElementById('ndpm-del')
+navItem.onclick = () => {
     
-});
+    if(navItem.classList.contains('active')){
+        navItem.classList.remove('active')
+        dpMenuList.classList.remove('dp-active')
+    } else{
+        navItem.classList.add('active')
+        dpMenuList.classList.add('dp-active')
+    }
 
+    
+        
+    
+}
 
-$('.content__slider').slick({
-    infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3
-  });
+navItemDelivery.onclick = () => {
+    
+    if(navItem.classList.contains('active')){
+        navItem.classList.remove('active')
+        dpMenuList.classList.remove('dp-active')
+    } else{
+        navItem.classList.add('active')
+        dpMenuList.classList.add('dp-active')
+    }
+
+    
+        
+    
+}
+
+  
